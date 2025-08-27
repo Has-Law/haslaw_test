e95ee93b3fec0ae668194ecb7e646560778c6d7a
santaimany <santaimany@gmail.com>
2025-08-28 02:17:42 +0700

    feat: replace buttons with links in AboutSec2 and Service components for improved navigation

--- src/components/landingpage/About-sec2.tsx
+++ src/components/landingpage/About-sec2.tsx
@@ -2,6 +2,7 @@
 import { useState } from "react";
 import Image from "next/image";
 import photo from "@/assets/landingpage/image/about.webp"; 
+import Link from "next/link";
 const AboutSec2 = () => {
     const [isHovered, setIsHovered] = useState(false);
     return(
@@ -12,12 +13,13 @@ const AboutSec2 = () => {
             <div className="flex flex-col gap-y-[2vw] py-[2vw]  items-start">
                 <h1 className="font_britanica_black text-[clamp(0.5rem,5vw,5rem)] text-[#131313]">Introducing <br />HAS Attorneys at Law</h1>
                 <p className="font_britanica_regular text-[clamp(2vw,2vw,4rem)] text-[#131313]">For nearly a decade, HAS Attorneys at Law has established itself as a trusted full-service law firm with a formidable reputation in litigation. Our primary strength lies in navigating high-stakes disputes, from complex commercial cases to sensitive anti-corruption matters. Backed by a team of seasoned lawyers, we are committed to delivering strategic and effective legal solutions to protect your most critical interests.</p>
-                <button 
+                <Link
+                href="/about" 
                 onMouseEnter={() => setIsHovered(true)}
                 onMouseLeave={() => setIsHovered(false)}
                 className={`text-[#A0001B] font_britanica_black text-[clamp(2vw,2vw,4rem)] border-[#A0001B] border-[0.2vw] p-[1vw] rounded-full transition-colors duration-300 ease-in-out cursor-pointer  ${isHovered ? 'bg-[#A0001B] text-white' : 'bg-white'} `}
                 
-                >More About Us</button>
+                >More About Us</Link>
             </div>
         </div>
     )
--- src/components/landingpage/Service.tsx
+++ src/components/landingpage/Service.tsx
@@ -5,6 +5,7 @@ import servicebg from "@/assets/landingpage/background/service.webp";
 import handcuffs from "@/assets/landingpage/icon/handcuffs.webp";
 import dolar from "@/assets/landingpage/icon/dolar.webp";
 import administrator from "@/assets/landingpage/icon/administrator.webp";
+import Link from "next/link";
 const Service = () => {
     const [isHovered, setIsHovered] = useState(false);
     return (
@@ -28,10 +29,12 @@ const Service = () => {
                         <h2 className="font_britanica_black text-white text-[clamp(1.6vw,1.6vw,4rem)] text-center">Administrative litigation</h2>
                     </div>
                 </div>
-            <button 
-            onMouseEnter={() => setIsHovered(true)}
-            onMouseLeave={() => setIsHovered(false)}
-             className={` font_britanica_black text-[clamp(1.5vw,1.5vw,4rem)] border-white border-[0.2vw] p-[1vw] rounded-full transition-colors duration-300 ease-in-out cursor-pointer ${isHovered ? 'bg-white text-[#A0001B]' : 'text-white'}`}>See Our Services</button>       
+            <Link
+              href="/service"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              className={` font_britanica_black text-[clamp(1.5vw,1.5vw,4rem)] border-white border-[0.2vw] p-[1vw] rounded-full transition-colors duration-300 ease-in-out cursor-pointer ${isHovered ? 'bg-white text-[#A0001B]' : 'text-white'}`}>See Our Services
+            </Link>
             </div>
         </div>
     );

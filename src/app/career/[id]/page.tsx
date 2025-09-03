e781b7984cc62f288ffa7a29cad1c828060cb12a
farildzaky <alfarildzaky@gmail.com>
2025-09-03 20:45:50 +0700

    vercel

--- src/app/career/[id]/page.tsx
+++ src/app/career/[id]/page.tsx
@@ -1,20 +1,7 @@
-// app/career/[id]/page.tsx (Server Component - tanpa "use client")
 import { getOpenBatches } from "@/lib/career";
 import CareerBatchClient from "./Batch-list";
 
-// generateStaticParams harus di Server Component
-export async function generateStaticParams() {
-    try {
-        const batches = await getOpenBatches(); // Menggunakan fungsi yang sudah ada
-        
-        return batches.map((batch) => ({
-            id: batch.id.toString(), // Pastikan id dalam bentuk string
-        }));
-    } catch (error) {
-        console.error('Error generating static params for career:', error);
-        return []; // Return array kosong jika error
-    }
-}
+
 
 type Props = {
     params: Promise<{

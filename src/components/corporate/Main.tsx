37cb77dfed5d61dec700e398a4ad1ca6f74162c3
farildzaky <alfarildzaky@gmail.com>
2025-10-04 15:30:20 +0700

    refactor: services

--- src/components/corporate/Main.tsx
+++ src/components/corporate/Main.tsx
@@ -9,19 +9,13 @@ const Main = () => {
                     <div className="flex flex-row gap-x-[2vw] items-start w-[30vw]">
                         <p className="font_britanica_black text-[clamp(12vw,12vw,10rem)] sm:text-[clamp(5vw,5vw,10rem)] leading-none w-[8vw]">1</p>
                         <h2 className="font_britanica_black text-[clamp(2.5vw,3.5vw,3rem)] sm:text-[clamp(2.5vw,2.5vw,3rem)] leading-tight sm:w-[15vw]">
-                            White Collar Defense
+                            Corporate Secretarial Services
                         </h2>
                     </div>
                     <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            Facing accusations of corporate or financial misconduct can be overwhelming, putting your reputation and future at risk. In Indonesia, these cases often involve complex investigations by agencies like the KPK (Komisi Pemberantasan Korupsi/Corruption Eradication Commission), Police, or Attorney General&apos;s Office. We&apos;re experts at navigating these challenges, from initial inquiries to the prosecution phase.
-                        </p>
-                        <p>
-                            We provide strong and smart defense for individuals and companies caught in white-collar cases, like fraud, embezzlement, insider trading, and corruption. We act fast and proactively, gathering evidence and developing robust defense strategies to protect your position.
-                        </p>
-                        <p>
-                            Our goal is to reduce risks at every step. We aim for quick solutions through talks, or if needed, we advocate strongly for you in court. We know the stakes are high and are fully committed to protecting your rights and achieving the best possible results in these sensitive situations.
-                        </p>
+                            Efficient, stable, compliant entities and structures are the foundations of business success. We ensure that the necessary paperwork is provided to the boards ahead of meetings and that board procedures are abided by. We also assist companies in complying with the prevailing laws and regulations and proper conduct within company activities. We also inform our clients with updates on laws and regulations so that they can procure necessary action to keep their businesses interests.                        </p>
+
                     </div>
                 </div>
 
@@ -29,19 +23,13 @@ const Main = () => {
                     <div className="flex flex-row gap-x-[2vw] items-start w-[30vw]">
                         <p className="font_britanica_black text-[clamp(12vw,12vw,10rem)] sm:text-[clamp(5vw,5vw,10rem)] leading-none w-[8vw]">2</p>
                         <h2 className="font_britanica_black text-[clamp(2.5vw,3.5vw,3rem)] sm:text-[clamp(2.5vw,2.5vw,3rem)] leading-tight w-[15vw]">
-                            Judicial Review
+                            Merger and Acquisition
                         </h2>
                     </div>
                     <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            Judicial review is a key way to make sure public bodies act legally and are held accountable in Indonesia. It allows people and organizations to challenge regulations or decisions by government bodies – like ministerial regulations or administrative acts – if they are unlawful, illogical, or procedurally unfair. This includes reviews by the Supreme Court (Mahkamah Agung) on lower regulations, or appeals from administrative court decisions.
-                        </p>
-                        <p>
-                            Our firm has deep knowledge in handling judicial review cases in various public law situations. We carefully look at your case details against public law principles, finding reasons like acting beyond legal powers, unfair procedures, or unreasonable decisions. We help clients through the strict rules and tight deadlines of judicial review.
-                        </p>
-                        <p>
-                            We are committed to helping clients uphold the rule of law and protect their rights against unfair or illegal government actions. Whether you want to overturn a wrong decision, make an agency act, or stop an illegal action, our team offers smart advice and strong representation throughout the judicial review process, from initial steps to full court hearings.
-                        </p>
+                            The business has to adjust occasionally to meet global needs and standards. HAS Attorneys at Law has always been trusted by our clients to enable the resources of their companies sustainably and sufficiently. We assist companies through all kinds of instruments and help our clients to channel through our wide range of networks.                        </p>
+
                     </div>
                 </div>
 
@@ -49,19 +37,27 @@ const Main = () => {
                     <div className="flex flex-row gap-x-[2vw] items-start w-[30vw]">
                         <p className="font_britanica_black text-[clamp(12vw,12vw,10rem)] sm:text-[clamp(5vw,5vw,10rem)] leading-none w-[8vw]">3</p>
                         <h2 className="font_britanica_black text-[clamp(2.5vw,3.5vw,3rem)] sm:text-[clamp(2.5vw,2.5vw,3rem)] leading-tight sm:w-[15vw]">
-                            Corporate & Commercial
+                            Corporate Restructuring
                         </h2>
                     </div>
                     <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            Beyond solving disputes, a business&apos;s success relies on solid legal foundations and smart commercial operations. Our corporate and commercial services provide full legal support to protect your business, ensure you follow rules, and help you grow from start-up to complex deals. We do this by truly understanding your business model and market.
-                        </p>
+                            HAS Attorneys at Law has always been trusted to involve in mergers and acquisitions. With a deep understanding of our client’s business interests, we have witnessed many successful corporate mergers and acquisitions. HAS Attorneys at Law can work with inter-functional professionals to deliver practical yet efficient results that win both ends.                        </p>
+
+                    </div>
+                </div>
+
+                <div className="flex flex-col sm:flex-row items-start text-[#4F000D] mb-[2vw]">
+                    <div className="flex flex-row gap-x-[2vw] items-start w-[30vw]">
+                        <p className="font_britanica_black text-[clamp(12vw,12vw,10rem)] sm:text-[clamp(5vw,5vw,10rem)] leading-none w-[8vw]">4</p>
+                        <h2 className="font_britanica_black text-[clamp(2.5vw,3.5vw,3rem)] sm:text-[clamp(2.5vw,2.5vw,3rem)] leading-tight sm:w-[15vw]">
+                            Capital Market
+                        </h2>
+                    </div>
+                    <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            We offer extensive guidance on all parts of corporate law. This includes setting up your company (e.g., Limited Liability Companies/Perseroan Terbatas), writing and negotiating many types of commercial contracts (like agreements for supply, distribution, or services), and ensuring robust corporate governance. We also assist with crucial administrative and compliance tasks, including corporate secretarial services, to ensure your company adheres to all statutory requirements, manages its corporate records diligently, and maintains good standing with relevant authorities.
-                        </p>
-                        <p>
-                            Our team also gives strategic advice on mergers, acquisitions, sales, and other complex corporate deals. We oversee everything from checking details to negotiating and closing deals, making sure transactions are smooth and legally sound. We are experienced in assisting a diverse range of clients, including State–Owned Enterprises (BUMN), domestic private companies (PT PMDN), and foreign investment companies (PT PMA), from small and medium–sized enterprises to multinational corporations. By combining our deep corporate knowledge with an awareness of business realities, we help your business operate securely, grow confidently, and reach its goals while avoiding legal problems.
-                        </p>
+                            As part of our comprehensive services to our clients, we now offer services in capital market field. With a registered legal consultant in capital market as our partner, HAS is able to provide various kinds of services concerning the capital market. HAS Attorneys at Law’s extensive experience enables us to give highly insightful advice in securities according to the client’s financial position.                        </p>
+
                     </div>
                 </div>
             </div>
--- src/components/litigation/Main.tsx
+++ src/components/litigation/Main.tsx
@@ -11,14 +11,9 @@ const Main = () => {
                     </div>
                     <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            Business disputes can seriously disrupt your operations and impact your financial health. These conflicts can come from many places, like broken contracts, disagreements between partners, shareholder issues, or claims of unfair competition. Solving them needs not just legal smarts, but also a good understanding of your business goals.
+                            We collaborate closely with clients to fully understand their challenges and deliver solutions aligned with business objectives. With a strong record in complex commercial litigation, our advocates combine courtroom advocacy with alternative approaches such as mediation and ADR, ensuring disputes are resolved efficiently while safeguarding valuable business relationships.
                         </p>
-                        <p>
-                            We specialize in representing businesses of all sizes in complex commercial lawsuits across different industries. We start by fully understanding the dispute, weighing the business impact against the legal facts. We always look for strategic ways to resolve issues, often using mediation or arbitration for faster and more affordable solutions, keeping your business running smoothly.
-                        </p>
-                        <p>
-                            When going to court is unavoidable, our skilled lawyers are ready to fiercely defend your interests. We build strong litigation plans, manage evidence carefully, and present convincing arguments to judges in court. Our focus remains on achieving solutions that protect your business, bring back stability, and help you thrive.
-                        </p>
+
                     </div>
                 </div>
                 <div className="flex flex-col sm:flex-row items-start text-[#4F000D]">
@@ -28,14 +23,9 @@ const Main = () => {
                     </div>
                     <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            When your freedom and reputation are on the line, you need powerful legal help. Criminal charges, big or small, can have huge impacts on you and your family. Handling the criminal justice system takes not just legal knowledge, but also a caring yet determined approach.
+                            We help clients navigate Indonesia’s dual legal system by reviewing government actions that impact individual and corporate rights. From securing permits and licenses to defending against administrative lawsuits, we offer strategic guidance, practical solutions, and effective representation to protect our clients’ interests before government authorities and regulatory bodies.
                         </p>
-                        <p>
-                            We represent clients across all types of criminal charges. Our service covers every part of the process, from thorough investigations before trial to carefully checking evidence and finding any mistakes in procedure.
-                        </p>
-                        <p>
-                            If your case goes to trial, our experienced litigators are ready to defend you vigorously. We&apos;re dedicated to protecting your rights, challenging overreach by prosecutors, and making sure your voice is heard. Our main goal is to get the best outcome for you, whether it&apos;s an acquittal, dismissal, or lighter penalties, so you can move forward.
-                        </p>
+
                     </div>
                 </div>
                 <div className="flex flex-col sm:flex-row items-start text-[#4F000D] mb-[2vw]">
@@ -45,14 +35,20 @@ const Main = () => {
                     </div>
                     <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            Engaging with government agencies and regulatory bodies is a common part of business, but it can lead to tough disputes and legal actions. Administrative litigation means challenging or defending decisions made by government bodies, handling complex rules, and making sure you follow the law while protecting your rights.
+                            Our experienced team responds to crises with speed and precision, guiding clients through challenging, high-stakes cases. By combining creative legal strategies, thorough preparation, and strategic courtroom advocacy, we secure favorable results and help clients manage complex risks in both criminal and white-collar matters.
                         </p>
+                    </div>
+                </div>
+                <div className="flex flex-col sm:flex-row items-start text-[#4F000D]">
+                    <div className="flex flex-row gap-x-[2vw] items-start w-[30vw]">
+                        <p className="font_britanica_black text-[clamp(12vw,12vw,10rem)] sm:text-[clamp(5vw,5vw,10rem)] leading-none w-[8vw]">4</p>
+                        <h2 className="font_britanica_black text-[clamp(2.5vw,3.5vw,3rem)] sm:text-[clamp(2.5vw,2.5vw,3rem)] leading-tight w-[15vw]">Tax Litigation Services</h2>
+                    </div>
+                    <div className="font_britanica_bold sm:font_britanica_regular text-[clamp(1.5vw,2.5vw,2rem)] sm:text-[clamp(1.5vw,1.5vw,1.5rem)] space-y-[1.5vw] text-justify sm:w-[40vw]">
                         <p>
-                            We offer expert legal help to individuals, businesses, and groups facing disputes with administrative agencies at all levels. Our services include challenging bad administrative decisions, appealing fines, seeking judicial review of agency actions, and representing you in enforcement cases.
+                            We represent clients in diverse tax disputes by combining legal expertise with practical strategies. Our team manages audits, objections, appeals, and judicial reviews while advising on mediation and arbitration to minimize risks. Skilled at resolving conflicts early, we help clients achieve favorable outcomes, avoid unnecessary disputes, and maintain compliance with tax authorities.
                         </p>
-                        <p>
-                            Our aim is to guide you through the often-confusing administrative legal world, giving you clear direction. We carefully prepare your case, argue effectively before administrative tribunals, and if needed, take your case to higher courts to ensure government actions are lawful and fair.
-                        </p>
+
                     </div>
                 </div>
             </div>

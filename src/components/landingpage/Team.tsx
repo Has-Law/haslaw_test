94d470cdd464541e6c8f4fe9d5395c4d09341234
farildzaky <alfarildzaky@gmail.com>
2025-10-30 21:34:32 +0700

    fix styling

--- src/components/landingpage/Team.tsx
+++ src/components/landingpage/Team.tsx
@@ -19,7 +19,7 @@ const Team = () => {
     ];
 
     return (
-        <div className="flex flex-col items-center gap-y-[1.6vw]  w-[90%] sm:w-[60%] mx-auto p-8">
+        <div className="flex flex-col items-center gap-y-[1.6vw]  w-[90%]  sm:w-[65%] mx-auto p-8">
             <h1 className="font_britanica_black text-[#131313] text-4xl sm:text-[clamp(1.5rem,4vw,3rem)]">Our Team</h1>
             <div className="flex flex-col items-center w-full">
                 
@@ -35,7 +35,7 @@ const Team = () => {
 
               
                     <div className={`hidden sm:flex flex-row px-6 sm:px-[4vw] items-center py-6 sm:py-[3vw] ${hoveredSection === 'partner' ? 'justify-between' : 'justify-center'}`}>
-                        <h2 className={`z-10 font_britanica_black text-white text-2xl sm:text-[clamp(0.4rem,3vw,2.4rem)] py-4 sm:py-[1.6vw] absolute transition-all duration-500 ease-in-out ${hoveredSection === 'partner' ? 'left-6 sm:left-[4vw] -translate-x-0' : 'left-1/2 -translate-x-1/2'}`}>
+                        <h2 className={`z-10 font_britanica_black text-white text-2xl sm:text-[clamp(2.5vw,2.5vw,2.4rem)] py-4 sm:py-[1.6vw] absolute transition-all duration-500 ease-in-out ${hoveredSection === 'partner' ? 'left-6 sm:left-[4vw] -translate-x-0' : 'left-1/2 -translate-x-1/2'}`}>
                              PARTNER
                         </h2>
                         <div className="relative z-10 ml-auto">
@@ -54,7 +54,7 @@ const Team = () => {
                     </div>
 
          
-                    <div className="sm:hidden flex flex-col px-6 py-2">
+                    <div className="sm:hidden flex flex-col  py-2">
                         <div className="flex justify-center">
                             <h2 className="z-10 font_britanica_black text-white text-2xl py-4">
                                  PARTNER
@@ -65,7 +65,7 @@ const Team = () => {
                                 <div className="overflow-hidden">
                                     <div className="flex flex-col gap-y-2 ">
                                         {partners.map((partner, index) => (
-                                            <h3 key={index} className="font_britanica_bold text-white text-center text-lg">
+                                            <h3 key={index} className="font_britanica_bold text-white text-center text-clamp(3vw,3vw,5vw)">
                                                 {partner.name}
                                             </h3>
                                         ))}
@@ -88,7 +88,7 @@ const Team = () => {
                     
          
                     <div className={`hidden sm:flex flex-row px-6 sm:px-[4vw] items-center py-6 sm:py-[3vw] ${hoveredSection === 'senior' ? 'justify-between' : 'justify-center'}`}>
-                        <h2 className={`z-10 font_britanica_black text-white text-2xl sm:text-[clamp(0.4rem,3vw,2.4rem)] py-4 sm:py-[1.6vw] absolute transition-all duration-500 ease-in-out ${hoveredSection === 'senior' ? 'left-6 sm:left-[4vw] -translate-x-0' : 'left-1/2 -translate-x-1/2'}`}>
+                        <h2 className={`z-10 font_britanica_black text-white text-2xl sm:text-[clamp(2.5vw,2.5vw,2.4rem)] py-4 sm:py-[1.6vw] absolute transition-all duration-500 ease-in-out ${hoveredSection === 'senior' ? 'left-6 sm:left-[4vw] -translate-x-0' : 'left-1/2 -translate-x-1/2'}`}>
                            SENIOR PARTNER
                         </h2>
                         <div className="relative z-10 ml-auto">
@@ -106,7 +106,7 @@ const Team = () => {
                         </div>
                     </div>
 
-                    <div className="sm:hidden flex flex-col px-6 py-2">
+                    <div className="sm:hidden flex flex-col  py-2">
                         <div className="flex justify-center">
                             <h2 className="z-10 font_britanica_black text-white text-2xl py-4">
                                SENIOR PARTNER
@@ -117,7 +117,7 @@ const Team = () => {
                                 <div className="overflow-hidden">
                                     <div className="flex flex-col gap-y-2 ">
                                         {seniorPartners.map((partner, index) => (
-                                            <h3 key={index} className="font_britanica_bold text-white text-center text-lg">
+                                            <h3 key={index} className="font_britanica_bold text-white text-center text-clamp(0.8rem,3vw,1.2rem)">
                                                 {partner.name}
                                             </h3>
                                         ))}
@@ -133,7 +133,7 @@ const Team = () => {
                     href="/member"
                     onMouseEnter={() => setHoveredSection('discover')}
                     onMouseLeave={() => setHoveredSection(null)}
-                    className="relative overflow-hidden font_britanica_black text-white flex justify-center text-2xl sm:text-[clamp(0.4rem,3vw,2.4rem)] w-full items-center rounded-b-xl sm:rounded-b-[1.6vw] py-4  sm:py-[1.6vw]"
+                    className="relative overflow-hidden font_britanica_black text-white flex justify-center text-2xl sm:text-[clamp(2.5vw,2.5vw,2.4rem)] w-full items-center rounded-b-xl sm:rounded-b-[1.6vw] py-4  sm:py-[1.6vw]"
                 >
                     <div className={`absolute inset-0 bg-[linear-gradient(90deg,_#A0001B_0%,_#3A000A_100%)] transition-opacity duration-300 ease-in-out ${hoveredSection === 'discover' ? 'opacity-0' : 'opacity-100'}`} />
                     <div className={`absolute inset-0 bg-[linear-gradient(270deg,_#A0001B_0%,_#3A000A_100%)] transition-opacity duration-300 ease-in-out ${hoveredSection === 'discover' ? 'opacity-100' : 'opacity-0'}`} />

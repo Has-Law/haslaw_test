a794c307ce1171a425d3e59690f624b6ecf5bdf1
farildzaky <alfarildzaky@gmail.com>
2025-10-31 09:42:20 +0700

    fix styling

--- src/app/achievements/page.tsx
+++ src/app/achievements/page.tsx
@@ -126,10 +126,10 @@ export default function Achievements() {
 
       <div className="relative z-10 w-full flex flex-col items-center">
         <div className="w-full flex flex-col items-center pt-[clamp(2rem,6vw,6rem)] pb-[clamp(1rem,3vw,2rem)] text-center">
-          <h1 className="text-white text-[clamp(2rem,6vw,4rem)] mt-[clamp(1rem,3vw,2.5rem)] font_britanica_black tracking-wide">
+          <h1 className="text-white text-[clamp(1.5rem,6vw,6rem)] sm:text-[clamp(1.8vw,3vw,4.5rem)] mt-[clamp(1rem,3vw,2.5rem)] font_britanica_black tracking-wide">
             Achievements
           </h1>
-          <p className="text-gray-200 text-[clamp(0.85rem,4vw,1.2rem)] sm:text-[clamp(2vw,2vw,5rem)] max-w-[clamp(80vw,80vw,60rem)] mt-4 font_britanica_regular px-[1vw] text-justify">
+          <p className="text-gray-200 text-[clamp(0.85rem,4vw,1.2rem)] sm:text-[clamp(1vw,2vw,1.25rem)] max-w-4xl mt-4 font_britanica_regular px-[1vw] text-justify">
             Our commitment to excellence is consistently recognized by the
             industry. Below are some of the awards and achievements that mark
             our dedication to providing premier legal services.

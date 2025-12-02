c57db0dbf5166df97ff7ad52e4b4798ad6f01c58
farildzaky <alfarildzaky@gmail.com>
2025-12-02 19:11:38 +0700

    fix: replace hero-mobile.png with hero-mobile.webp and adjust padding in Achievements component

--- src/components/achievment/main.tsx
+++ src/components/achievment/main.tsx
@@ -133,7 +133,7 @@ export default function Achievements() {
           <h1 className="text-white text-[clamp(1.5rem,6vw,6rem)] sm:text-[clamp(1.8vw,3vw,4.5rem)] mt-[clamp(1rem,3vw,2.5rem)] font_britanica_black tracking-wide">
             Achievements
           </h1>
-          <p className="text-gray-200 font_britanica_regular text-[clamp(0.85rem,4vw,1.2rem)] sm:text-[clamp(1vw,2vw,1.5rem)] text-justify text-[#131313] max-w-6xl">
+          <p className="text-gray-200 font_britanica_regular text-[clamp(0.85rem,4vw,1.2rem)] sm:text-[clamp(1vw,2vw,1.5rem)] px-[2vw] text-justify text-[#131313] max-w-6xl">
             Our commitment to excellence is consistently recognized by the
             industry. Below are some of the awards and achievements that mark
             our dedication to providing premier legal services.
